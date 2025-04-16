5e5ce64163fa0ad27a641cd00583fd4da6ad9e0e
Patrick Hilgenstock <pahilgenstock@hotmail.de>
2025-04-16 08:37:16 +0200

    Use streams

--- src/main/java/de/philgenstock/ssp/ScissorStonePaper.java
+++ src/main/java/de/philgenstock/ssp/ScissorStonePaper.java
@@ -1,10 +1,11 @@
 package de.philgenstock.ssp;
 
-import javax.print.attribute.standard.PrinterMakeAndModel;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ScissorStonePaper {
 
@@ -15,17 +16,23 @@ public class ScissorStonePaper {
     }
 
     public Map<Result, Integer> play(int times) {
-        EnumMap<Result, Integer> resultMap = new EnumMap<>(Result.class);
-        for (Result value : Result.values()) {
-            resultMap.put(value, 0);
+
+        EnumMap<Result, Integer> resultMap = IntStream.range(0, times)
+                .mapToObj(i -> {
+                    Shape attackingShape = Shape.PAPER;
+                    Shape defendingShape = Shape.values()[random.nextInt(Shape.values().length)];
+                    return attackingShape.calculateResultAgainst(defendingShape);
+                })
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        () -> new EnumMap<>(Result.class),
+                        Collectors.collectingAndThen(Collectors.counting(), Long::intValue)
+                ));
+
+        for (Result result : Result.values()) {
+            resultMap.putIfAbsent(result, 0);
         }
-        for (int i = 0; i < times; i++) {
-            Shape attackingShape = Shape.PAPER;
-            Shape defendingShape = Shape.values()[random.nextInt(Shape.values().length)];
-            Result result = attackingShape.calculateResultAgainst(defendingShape);
-            int totalResultCount = resultMap.get(result) + 1;
-            resultMap.put(result, totalResultCount);
-        }
+
         return resultMap;
     }
 }
